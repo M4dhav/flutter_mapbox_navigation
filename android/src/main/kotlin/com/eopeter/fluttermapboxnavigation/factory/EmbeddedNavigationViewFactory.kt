3052f7d7d2c3035dab6b9f4aac5c7c7ed499a24b
Madhav Gupta <madhav.gupta7674@gmail.com>
2024-11-27 23:34:09 +0530

    AppCompat Fix try2

--- android/src/main/kotlin/com/eopeter/fluttermapboxnavigation/factory/EmbeddedNavigationViewFactory.kt
+++ android/src/main/kotlin/com/eopeter/fluttermapboxnavigation/factory/EmbeddedNavigationViewFactory.kt
@@ -12,28 +12,12 @@ import io.flutter.plugin.common.StandardMessageCodec
 import io.flutter.plugin.platform.PlatformView
 import io.flutter.plugin.platform.PlatformViewFactory
 
-class EmbeddedNavigationViewFactory(
-    private val messenger: BinaryMessenger,
-    private val activity: Activity
-) : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
+class EmbeddedNavigationViewFactory(private val messenger: BinaryMessenger, private val activity: Activity) : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
     override fun create(context: Context?, viewId: Int, args: Any?): PlatformView {
-        val inflater = LayoutInflater.from(context)
-        val binding = NavigationActivityBinding.inflate(inflater)
-        val accessToken = PluginUtilities.getResourceFromContext(context, "mapbox_access_token")
-        val view = EmbeddedNavigationMapView(
-            context!,
-            activity,
-            binding,
-            messenger,
-            viewId,
-            args,
-            accessToken
-        )
-
-        view.initialize()
-
+        val binding = NavigationActivityBinding.inflate(this.activity.layoutInflater)
+        val accessToken = PluginUtilities.getResourceFromContext( context!!,"mapbox_access_token")
+        val view = EmbeddedNavigationMapView(context!!, activity, binding, messenger, viewId, args, accessToken)
         activity.setTheme(R.style.Theme_AppCompat_NoActionBar)
-
         return view
     }
 }
